00ad49d9d34dce7af2ab1290ef39846fbec6f5c9
piljae <piljaekang98@naver.com>
2023-02-02 04:00:00 +0900

    chatting fragment 완성

--- app/src/main/java/com/example/myapp/ChattingFragment.kt
+++ app/src/main/java/com/example/myapp/ChattingFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapp.databinding.FragmentChattingBinding
 import com.example.myapp.databinding.FragmentMapBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -17,7 +22,12 @@ class ChattingFragment : Fragment() {
 
 
 
-    lateinit var binding : FragmentMapBinding
+
+    private var _binding: FragmentChattingBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
     lateinit var adapter : userAdapter
 
     private lateinit var mAuth: FirebaseAuth
@@ -33,9 +43,8 @@ class ChattingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        val view = inflater.inflate(R.layout.fragment_chatting, container, false)
-
-        binding = FragmentMapBinding.inflate(inflater, container, false)
+        _binding = FragmentChattingBinding.inflate(inflater, container, false)
+        val view = binding.root
 
         //인증 초기화
         mAuth = Firebase.auth
@@ -44,16 +53,40 @@ class ChattingFragment : Fragment() {
         mDbRef = Firebase.database.reference
 
 
-        adapter = userAdapter(this, userList)
+        mDbRef.child("user").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
 
+                for (postSnapshot in snapshot.children) {
 
+                    val currentUser = postSnapshot.getValue(User::class.java)
 
+                    if(mAuth.currentUser?.uid != currentUser?.uId){
+                        userList.add(currentUser!!)
+                    }
 
+                }
+                adapter.notifyDataSetChanged()
 
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //실패시 실행
+            }
+        })
+
+        adapter = userAdapter(userList, requireContext())
+
+        binding.userRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.userRecyclerView.adapter = userAdapter(userList, requireContext())
 
 
 
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
